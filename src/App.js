a2fc3ebdbacdab7aff628f8afdc89887591cbf56
linkluu <aliwa0202@gmail.com>
2021-12-30 21:46:07 +0600

    load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Header/>
+     <div class="row">
+    			<div class="col-lg-9">
+    				<form class="action text-center">
+    					<div class="form-row">
+    						<div class="col-lg-6">
+    							<input type=""class="form-control bg-light mb-3"placeholder="Name" name="">
+    						</div>
+    						<div class="col-lg-6">
+    							<input type=""class="form-control bg-light mb-3"placeholder="Name" name="">
+    						</div>
+    					</div>
+    					<div class="form-row">
+    						<div class="col-lg-12">
+    							<textarea class="form-control bg-light mb-3" placeholder="Message"cols="30" rows="10"></textarea>
+    						</div>
+    					</div>
+    					<button type="button" class="btn btn-success">Submit</button>
+    				</form>
+    			</div>
+    		</div>
+
+     < Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+
+
+function Footer() {
+    return (
+      <div class="footer">
+        <h1 class="text"> 2021 Ala-Too International University</h1>
+      </div>
+    );
+  }
+  
+  export default Footer;
